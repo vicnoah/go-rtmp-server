b4ebab098f57e591ac6eb71e8e86ff1aafc0876d
huzhifeng <huzhifeng@douyu.tv>
2017-07-14 14:51:56 +0800

    Fix style, background default black

--- main.go
+++ main.go
@@ -118,16 +118,34 @@ func main() {
 		body {
 			margin:0;
 			padding:0;
+			background:#000;
 		}
-		video {
-			width:100%;
-			height:100%;
-		}
+		
+        .mainContainer {
+            display: block;
+            width: 1280px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .centeredVideo {
+            display: block;
+            width: 100%;
+			height: 720px;
+			margin-top:50px;
+            margin-left: auto;
+            margin-right: auto;
+            margin-bottom: auto;
+        }
 		</style>
 	</head>
 	<body>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/flv.js/1.3.2/flv.min.js"></script>
-		<video id="videoElement"></video>
+		   <div class="mainContainer">
+        <video id="videoElement" name="videoElement" class="centeredVideo" controls autoplay width="1024" height="576">
+            Your browser is too old which doesn't support HTML5 video.
+        </video>
+    </div>
 		<script>
 if (flvjs.isSupported()) {
 	var videoElement = document.getElementById('videoElement');
